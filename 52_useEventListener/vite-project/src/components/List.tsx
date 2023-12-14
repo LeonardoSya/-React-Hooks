f33c80978cecb0183b1b3b4d768bba8e66052cc8
LeonardoSya <2246866774@qq.com>
2023-12-14 22:38:22 +0800

    modified:   52_useEventListener/vite-project/src/components/List.tsx 	modified:   52_useEventListener/vite-project/src/components/List.tsx

--- 52_useEventListener/vite-project/src/components/List.tsx
+++ 52_useEventListener/vite-project/src/components/List.tsx
@@ -1,3 +1,11 @@
+/*
+ * @Author: LeonardoSya 2246866774@qq.com
+ * @Date: 2023-12-14 15:13:19
+ * @LastEditors: LeonardoSya 2246866774@qq.com
+ * @LastEditTime: 2023-12-14 22:36:21
+ * @FilePath: \React\深入理解React Hooks\52_useEventListener\vite-project\src\components\List.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import React from 'react';
 import ListItem from './ListItem';
 
